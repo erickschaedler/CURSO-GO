3ff3aa963faa3eed85f0189e88db2b001e02d54e
Erick Schaedler <desenv10@idata.com.br>
2025-03-06 06:44:39 -0300

    trabalhando com json

--- pacotes-importantes/trabalhando-com-json/main.go
+++ pacotes-importantes/trabalhando-com-json/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main()  {
-	
+import (
+	"encoding/json"
+	"os"
+)
+
+type Conta struct {
+	Numero int `json:"n"`
+	Saldo  int `json:"s"`
+}
+
+func main() {
+	conta := Conta{Numero: 1, Saldo: 1}
+
+	res, err := json.Marshal(conta)
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(res))
+
+	err = json.NewEncoder(os.Stdout).Encode(conta)
+	if err != nil {
+		panic(err)
+	}
+
+
+	jsonPuro := []byte(`{"n":2,"s":2}`)
+
+	var contaX Conta
+	err = json.Unmarshal(jsonPuro, &contaX)
+	if err != nil {
+		panic(err)
+	}
+
+	println(contaX.Saldo)
 }
