b027c6ba789f1718ac0aee53a37a4504ecffcb3d
Erick Schaedler <desenv10@idata.com.br>
2025-03-12 17:10:47 -0300

    feat: server mux and file server

---
+++ pacotes-importantes/server-mux/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", HomeHandler)
+	mux.Handle("/blog", blog{title: "My blog"})
+
+	http.ListenAndServe(":8080", mux)
+
+	mux2 := http.NewServeMux()
+	mux2.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello Erick"))
+	})
+	http.ListenAndServe(":8081", mux2)
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
+
+type blog struct {
+	title string
+}
+
+func (b blog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(b.title))
+}
